5d7c8f2a6b26ee1e96a317b4b1b645d400d4fe1c
Surya Duggirala <surya.duggirala96@gmail.com>
2020-07-05 14:11:12 -0700

    Updates

---
+++ precache-manifest.e57a1c9e87ec99b860ac18309882304a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c05a893f9bf2b3d4b96a27798fb2b7",
+    "url": "/Catalogue-App/index.html"
+  },
+  {
+    "revision": "e29d6c99090f169464c5",
+    "url": "/Catalogue-App/static/css/main.e472027c.chunk.css"
+  },
+  {
+    "revision": "9f8d4650e61150e7acff",
+    "url": "/Catalogue-App/static/js/2.d5c4ca0e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Catalogue-App/static/js/2.d5c4ca0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e29d6c99090f169464c5",
+    "url": "/Catalogue-App/static/js/main.32ddb297.chunk.js"
+  },
+  {
+    "revision": "abc1c4aa88702d05837f",
+    "url": "/Catalogue-App/static/js/runtime-main.69b175ed.js"
+  }
+]);
